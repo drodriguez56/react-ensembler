bd8e21ba11a1038c2881baecf346d4a7b6cc91f4
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-03-23 20:07:20 -0400

    first commit

---
+++ app/Ensembler.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export default class Ensembler extends React.Component{
+		componenDidMount = () => {
+			
+		}
+    render() {
+        return(
+           	<div>
+           		<p>hello</p>
+           	</div>
+            )
+    }
+}
