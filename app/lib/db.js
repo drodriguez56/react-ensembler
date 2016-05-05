72b39bf3848bead97bc5de562288ea086ae0abad
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2016-05-05 09:45:58 -0400

    placeholder img array

---
+++ app/lib/db.js
@@ -0,0 +1,4 @@
+export function fetchSlides(cb) {
+    return fakeSlides
+}
+var fakeSlides = ["https:\/\/d7677ee7d66f831d2515-1c0040cb7de827f1f7b4fcca9328ef62.ssl.cf1.rackcdn.com\/spree\/products\/37\/large\/RG_Coat_Black.png?1445376138","https:\/\/d7677ee7d66f831d2515-1c0040cb7de827f1f7b4fcca9328ef62.ssl.cf1.rackcdn.com\/spree\/products\/34\/large\/PS_Hoodie.png?1445377277"]
